d9983c4a0058422a9fb7afd94d6b4e54b4939a07
Chris Mems <christopher.mems@.com>
2023-02-06 20:39:38 -0600

    add a comment

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,9 +9,10 @@ const router = Router()
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, blogsCtrl.create)
 router.get('/', checkAuth, blogsCtrl.index)
 router.get('/:id', checkAuth, blogsCtrl.show)
+router.post('/', checkAuth, blogsCtrl.create)
+router.post('/:id/comments', checkAuth, blogsCtrl.createComment)
 router.put('/:id', checkAuth, blogsCtrl.update)
 router.delete('/:id', checkAuth, blogsCtrl.delete)
 
